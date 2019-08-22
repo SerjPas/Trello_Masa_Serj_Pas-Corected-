1aaf8c83e7de059884c3533eff48a59e87944fcc
SerjPas <passergiy@gmail.com>
2019-08-22 13:11:56 +0300

    all works well

--- Trello-mobile/src/test/java/com/telran/trello/manager/HelloPage.java
+++ Trello-mobile/src/test/java/com/telran/trello/manager/HelloPage.java
@@ -1,9 +1,14 @@
 package com.telran.trello.manager;
 
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
 
 public class HelloPage extends HelperBase {
     public HelloPage(AppiumDriver driver) {
         super(driver);
+    }
+
+    public String getText() {
+        return driver.findElement(By.id("text")).getText();
     }
 }
--- Trello-mobile/src/test/java/com/telran/trello/tests/OpenAppTest.java
+++ Trello-mobile/src/test/java/com/telran/trello/tests/OpenAppTest.java
@@ -1,15 +1,21 @@
 package com.telran.trello.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class OpenAppTest  extends TestBase{
   @Test
   public void openApp() throws InterruptedException {
     System.out.println("app opened");
+    Assert.assertEquals(app.getHelloPage().getText(),"Hello Trello!");
 
     app.getHelloPage().swipeFromRightToLeft();
+    Assert.assertEquals(app.getHelloPage().getText(),"Get organized");
+   // Assert.assertTrue();
     app.getHelloPage().swipeFromRightToLeft();
+    Assert.assertEquals(app.getHelloPage().getText(),"Add details");
     app.getHelloPage().swipeFromRightToLeft();
+    Assert.assertEquals(app.getHelloPage().getText(),"Team up");
 
 
 
